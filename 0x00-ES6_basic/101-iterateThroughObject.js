8a756ae7ab02611c843d390d6db42cb588ef95f3
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-06-26 19:50:35 +0100

    ES basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let result = '';
+    for (const employee of reportWithIterator.allEmployees) {
+        result += `${employee} `;
+    }
+    return result;
+}
