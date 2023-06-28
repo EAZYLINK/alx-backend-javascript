dd75fdffca7a9257b9fa36f0dd0faebe160dd04c
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-06-28 13:56:55 +0100

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,7 +0,0 @@
-export default function iterateThroughObject (reportWithIterator) {
-  let result = ''
-  for (const employee of reportWithIterator.allEmployees) {
-    result += `${employee} `
-  }
-  return result
-}
